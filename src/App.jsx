815e29764e73f723c3a1abde940721f90a86eaaf
sheymantech <olaniyansheyi1704@gmail.com>
2024-04-02 17:58:42 +0100

    new feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+
+createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+]);
+
 function App() {
   const x = 23;
   return <div>Hello Vite</div>;
