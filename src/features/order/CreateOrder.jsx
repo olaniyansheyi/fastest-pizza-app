b9405448fa47bb42652668b885cbeaedff38999a
sheymantech <olaniyansheyi1704@gmail.com>
2024-04-03 20:26:26 +0100

    new feature

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { Form } from "react-router-dom";
+import { Form, redirect } from "react-router-dom";
+import { createOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -91,7 +92,9 @@ export async function action({ request }) {
     priority: data.priority === "on",
   };
 
-  return null;
+  const newOrder = await createOrder(order);
+
+  return redirect(`/order/${newOrder.id}`);
 }
 
 export default CreateOrder;
