018d411d7f46ae33b6eab50f46bb1e79647ed31d
sheymantech <olaniyansheyi1704@gmail.com>
2024-04-05 19:17:08 +0100

    new feature

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -10,12 +10,14 @@ function AppLayout() {
   const isLoading = navigation.state === "loading";
 
   return (
-    <div className="layout">
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]  ">
       {isLoading && <Loader />}
       <Header />
-      <main>
+      <div className="overflow-scroll ">
+      <main className=" mx-auto max-w-3xl ">
         <Outlet />
       </main>
+     </div>
       <CartOverview />
     </div>
   );
--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -2,7 +2,7 @@ import CreateUser from '../features/user/CreateUser';
 
 function Home() {
   return (
-    <div className='my-10 sm:my-10 text-center'>
+    <div className='my-10 sm:my-10 text-center px-4'>
       <h1 className=" text-xl font-semibold mb-8">
         The best pizza.
         <br />
