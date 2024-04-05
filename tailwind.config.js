8e713df73986ed5b6121146a54a5e170961b1c7e
sheymantech <olaniyansheyi1704@gmail.com>
2024-04-06 00:19:51 +0100

    new feature

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+ 
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: 'Roboto Mono, momospace',
+    },
+    extend: {
+      height: {
+        screen: '100dvh'
+      }
+    },
+
   },
   plugins: [],
 };
