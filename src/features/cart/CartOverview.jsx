124cdc7297266eb02bdf8aee2061ea3e1dafe360
sheymantech <olaniyansheyi1704@gmail.com>
2024-04-05 18:29:48 +0100

    new feature

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function CartOverview() {
   return (
-    <div className="bg-stone-800 uppercase px-4 text-sm md:text-base py-4 text-stone-200 sm:px-6">
+    <div className="bg-stone-800 uppercase px-4 text-sm md:text-base py-4 text-stone-200 sm:px-6 flex items-center justify-between">
       <p className="text-stone-300 space-x-4 ms:space-x-6 font-semibold">
         <span>23 pizzas</span>
         <span>$23.45</span>
