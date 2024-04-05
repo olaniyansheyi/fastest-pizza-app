d97e001a377b89f4de7ec4e2551052b473edaf7a
sheymantech <olaniyansheyi1704@gmail.com>
2024-04-05 19:51:41 +0100

    new feature

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -79,7 +79,8 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting}>
+          <button disabled={isSubmitting} className="bg-yellow-400 uppercase font-semibold text-stone-800 py-3  tracking-wide px-4 rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2
+          disabled:cursor-not-allowed">
             {isSubmitting ? "placing order...." : "order now"}
           </button>
         </div>
