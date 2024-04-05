bda3a6bacdfaab3b61975bafd0c2b796a3d6961b
sheymantech <olaniyansheyi1704@gmail.com>
2024-04-05 23:24:51 +0100

    new feature

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,5 +1,7 @@
 function Loader() {
-  return <div className="loader"></div>;
+  return <div className="absolute bg-slate-200/20 inset-0 backdrop-blur-sm flex items-center justify-center">
+    <div className="loader"></div>
+  </div>
 }
 
 export default Loader;
