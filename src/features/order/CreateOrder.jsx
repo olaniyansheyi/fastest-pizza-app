b4811fe6a522e0fae4674d4602627553dced6584
sheymantech <olaniyansheyi1704@gmail.com>
2024-04-06 23:07:11 +0100

    new feature

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -43,32 +43,33 @@ function CreateOrder() {
   const cart = fakeCart;
 
   return (
-    <div>
-      <h2>Ready to order? Let go!</h2>
+    <div className="px-4 py-6">
+      <h2 className="mb-8 font-semibold text-xl ">Ready to order? Let go!</h2>
 
       <Form method="POST">
-        <div>
-          <label>First Name</label>
-          <input type="text" name="customer" required className="input"  />
+        <div className="mb-5 flex gap-2 flex-col sm:flex-row sm:items-center">
+          <label className="sm:basis-40">First Name</label>
+          <input type="text" name="customer" required className="input grow "  />
         </div>
 
-        <div>
-          <label>Phone number</label>
-          <div>
-            <input type="tel" name="phone" required className="input"  />
+        <div className="mb-5 flex gap-2 flex-col sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Phone number</label>
+          <div className="grow">
+            <input type="tel" name="phone" required className="input w-full" />
+            {formErrors?.phone && <p className=" text-xs p-2 mt-2 text-red-700 bg-red-100 rounded-md">{formErrors.phone}</p>}
           </div>
-          {formErrors?.phone && <p>{formErrors.phone}</p>}
+          
         </div>
 
-        <div>
-          <label>Address</label>
-          <div>
+        <div className="mb-5 flex gap-2 flex-col sm:flex-row sm:items-center">
+          <label className="basis-40">Address</label>
+          <div className="grow">
             <input type="text" name="address"
-            className="input"  required />
+            className="input w-full"  required />
           </div>
         </div>
 
-        <div>
+        <div className="mb-12 flex gap-5 items-center">
           <input
             type="checkbox"
             name="priority"
@@ -78,7 +79,7 @@ function CreateOrder() {
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="priority">Want to yo give your order priority?</label>
+          <label htmlFor="priority" className="font-medium">Want to yo give your order priority?</label>
         </div>
 
         <div>
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -5,13 +5,13 @@ function Button({ children, disabled, to, type }) {
     
 
 
-    const base = 'bg-yellow-400 uppercase font-semibold text-stone-800 tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed';
+    const base = ' text-sm bg-yellow-400 uppercase font-semibold text-stone-800 tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed';
     
     const styles = {
         primary: base + ' px-4 py-3 sm:px-6 sm:py-4',
 
         small: base + ' py-2 px-4 md:py-2.5 md:px-5 text-xs',
-        secondary: ' border-2 border-stone-300 uppercase font-semibold text-stone-800 tracking-wide rounded-full hover:bg-stone-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-stone-800 focus:bg-stone-200 focus:ring-offset-2 hover:text-stone-800 disabled:cursor-not-allowed py-2.5 px-4 md:px-6 md:py-3.5',
+        secondary: '  text-sm border-2 border-stone-300 uppercase font-semibold text-stone-800 tracking-wide rounded-full hover:bg-stone-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-stone-800 focus:bg-stone-200 focus:ring-offset-2 hover:text-stone-800 disabled:cursor-not-allowed py-2.5 px-4 md:px-6 md:py-3.5',
     }
     
     if (to) return <Link className={styles[type]} to={to}>{ children}</Link>
