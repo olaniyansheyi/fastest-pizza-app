fca2c0cd82d0cc71144c69b9a7548eea83ba629c
sheymantech <olaniyansheyi1704@gmail.com>
2024-04-06 22:28:00 +0100

    new feature

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -10,7 +10,8 @@ function Button({ children, disabled, to, type }) {
     const styles = {
         primary: base + ' px-4 py-3 sm:px-6 sm:py-4',
 
-        small: base + ' py-2 px-4 md:py-2.5 md:px-5 text-xs'
+        small: base + ' py-2 px-4 md:py-2.5 md:px-5 text-xs',
+        secondary: ' border-2 border-stone-300 uppercase font-semibold text-stone-800 tracking-wide rounded-full hover:bg-stone-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-stone-800 focus:bg-stone-200 focus:ring-offset-2 hover:text-stone-800 disabled:cursor-not-allowed py-2.5 px-4 md:px-6 md:py-3.5',
     }
     
     if (to) return <Link className={styles[type]} to={to}>{ children}</Link>
