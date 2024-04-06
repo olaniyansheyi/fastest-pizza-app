5511bcd0487ed6a69e7328676208703c8dd15a73
sheymantech <olaniyansheyi1704@gmail.com>
2024-04-06 20:01:46 +0100

    new feature

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -6,7 +6,7 @@ function Menu() {
   const menu = useLoaderData();
 
   return (
-    <ul>
+    <ul className="divide-y divide-stone-200 px-2">
       {menu.map((pizza) => (
         <MenuItem pizza={pizza} key={pizza.id} />
       ))}
--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -4,12 +4,12 @@ function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
   return (
-    <li>
-      <img src={imageUrl} alt={name} />
-      <div>
+    <li className="flex gap-4 py-2">
+      <img src={imageUrl} alt={name} className="h-24" />
+      <div className="flex flex-col">
         <p>{name}</p>
         <p>{ingredients.join(', ')}</p>
-        <div>
+        <div className="mt-auto">
           {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
         </div>
       </div>
