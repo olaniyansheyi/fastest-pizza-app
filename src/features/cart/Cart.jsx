7f6785dc7741c49e926b2034a4a111862ee73bad
sheymantech <olaniyansheyi1704@gmail.com>
2024-04-09 01:21:05 +0100

    new feature

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -2,35 +2,19 @@ import { Link } from 'react-router-dom';
 import LinkButton from '../../ui/LinkButton';
 import Button from '../../ui/Button';
 import CartItem from '../cart/CartItem'
-import { useSelector } from 'react-redux';
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: 'Mediterranean',
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: 'Vegetale',
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: 'Spinach and Mushroom',
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
+import { useSelector,useDispatch } from 'react-redux';
+import { clearCart, getCart } from './CartSlice';
+import EmptyCart from './EmptyCart'
+
 
 function Cart() {
-  const cart = fakeCart;
+  const cart = useSelector(getCart);
+  const dispach = useDispatch();
+  const userName = useSelector(store => store.user.userName);
+
+  if(!cart.length) return <EmptyCart />
  
-const userName = useSelector(store => store.user.userName);
+
 
   return (
     <div className=' px-4 py-3'>
@@ -44,7 +28,7 @@ const userName = useSelector(store => store.user.userName);
 
       <div className='mt-6 space-x-2'>
         <Button type='primary' to="/order/new">Order pizza</Button>
-        <Button type='secondary'>Clear cart</Button>
+        <Button type='secondary' onClick={()=> dispach(clearCart())}>Clear cart</Button>
       </div>
     </div>
   );
--- src/features/cart/CartSlice.js
+++ src/features/cart/CartSlice.js
@@ -44,3 +44,5 @@ export default cartSlice.reducer;
 export const getTotalCartQuantity = (state) => state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
 
 export const getTotalCartPrice = (state) => state.cart.cart.reduce((acc, item) => acc + item.totalPrice, 0);
+
+export const getCart = (state) => state.cart.cart;
