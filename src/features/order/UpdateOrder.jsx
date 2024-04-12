f668255e0dc7d483e268e3167efdc9d80b3485a1
sheymantech <olaniyansheyi1704@gmail.com>
2024-04-12 07:53:51 +0100

    new feature

--- src/features/order/UpdateOrder.jsx
+++ src/features/order/UpdateOrder.jsx
@@ -1,6 +1,7 @@
 
 import { useFetcher } from 'react-router-dom'
 import Button from '../../ui/Button'
+import { updateOrder } from '../../services/apiRestaurant';
 
 function UpdateOrder({ order }) {
 
@@ -13,6 +14,10 @@ function UpdateOrder({ order }) {
 export default UpdateOrder
 
  export async function action({ request, params }) {
-    console.log('update')
+   const data = {
+    priority: true
+  }
+
+   await updateOrder(params.orderId,data);
     return null;
 }
